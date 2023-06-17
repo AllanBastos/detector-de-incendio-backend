c5959e10dcca636f556b5ddbbb9ac64d3167b492
AYRtonMeD <ayrtondantas2013@gmail.com>
2023-06-17 19:33:14 -0300

    Criado projeto

---
+++ src/main/java/com/detectordeincendiobackend/DetectorDeIncendioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.detectordeincendiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorDeIncendioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectorDeIncendioBackendApplication.class, args);
+	}
+
+}
