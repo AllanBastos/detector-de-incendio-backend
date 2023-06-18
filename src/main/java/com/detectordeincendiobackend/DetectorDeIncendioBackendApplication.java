b2fe34207bf7bcf7a824d43d6e667e70b06d3969
AYRtonMeD <ayrtondantas2013@gmail.com>
2023-06-18 14:24:14 -0300

    Adicionado extensão!

--- src/main/java/com/detectordeincendiobackend/DetectorDeIncendioBackendApplication.java
+++ src/main/java/com/detectordeincendiobackend/DetectorDeIncendioBackendApplication.java
@@ -2,9 +2,10 @@ package com.detectordeincendiobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DetectorDeIncendioBackendApplication {
+public class DetectorDeIncendioBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DetectorDeIncendioBackendApplication.class, args);
